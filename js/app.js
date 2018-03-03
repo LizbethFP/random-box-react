a45c1fcf16cd4cddc25c7bc3e4c9602c8cdb4dee
LizbethFP <lizbeth.fp@gmail.com>
2018-03-03 17:53:36 -0500

    añadiendo colores y fuentes random

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const containerExample = () => {
+  let colors = ['red', 'green', 'blue', 'orange', 'yellow', 'grey', 'black', 'purple'];
+  let fonts = ['Mina', 'Karma', 'Indie Flower', 'Comfortaa', 'VT323', 'Anonymous Pro', 'Nanum Myeongjo', 'Krona One'];
   const Div = ({title, parragraph}) => (
-    <div>
+    <div style={{ backgroundColor: colors[Math.floor(Math.random() * colors.length)], fontFamily: fonts[Math.floor(Math.random() * fonts.length)]}}>
       <h1>{title}</h1>
       <p>{parragraph}</p>
     </div>
