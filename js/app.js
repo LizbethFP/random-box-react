8effff12069c263ce39dfd622afe4bddb7a62e81
LizbethFP <lizbeth.fp@gmail.com>
2018-03-03 16:38:32 -0500

    creando e renderizando componente

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+const containerExample = () => {
+  const Div = ({title, parragraph}) => (
+    <div>
+      <h1>{title}</h1>
+      <p>{parragraph}</p>
+    </div>
+  );
 
+  const containerProps = {
+    title: 'Este es un div con background random',
+    parragraph: 'Y fuente random'
+  };
+
+  ReactDOM.render(
+    <Div {...containerProps} />,
+    document.getElementById('app')
+  );
+
+};
+
+containerExample();
