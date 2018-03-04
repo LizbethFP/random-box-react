8182252db9e84dca3ae73e491a5a96c60b4d0a96
LizbethFP <lizbeth.fp@gmail.com>
2018-03-04 17:56:41 -0500

    añadiendo más colores y fuentes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const containerExample = () => {
 
-  let colors = ['red', 'green', 'blue', 'orange', 'yellow', 'grey', 'pink', 'purple'];
-  let fonts = ['Mina', 'Karma', 'Indie Flower', 'Comfortaa', 'VT323', 'Anonymous Pro', 'Nanum Myeongjo', 'Krona One'];
+  let colors = ['red', 'green', 'blue', 'orange', 'yellow', 'grey', 'pink', 'purple', 'BlueViolet ', 'CadetBlue', 'DeepPink', 'DeepSkyBlue', 'DarkTurquoise', 'IndianRed', 'MediumVioletRed', 'MidnightBlue', 'OrangeRed', 'PaleVioletRed', 'Salmon'];
+  let fonts = ['Mina', 'Karma', 'Indie Flower', 'Comfortaa', 'VT323', 'Anonymous Pro', 'Nanum Myeongjo', 'Krona One', 'Cutive Mono', 'Nanum Brush Script', 'Permanent Marker', 'Courgette', 'Tangerine', 'Handlee', 'Kalam', 'Rock Salt', 'Reenie Beanie'];
   
   const Div = ({title, parragraph}) => (
-    <div style={{ backgroundColor: colors[Math.floor(Math.random() * colors.length)], fontFamily: fonts[Math.floor(Math.random() * fonts.length)], padding:'2%'}}>
+    <div style={{ backgroundColor: colors[Math.floor(Math.random() * colors.length)], fontFamily: fonts[Math.floor(Math.random() * fonts.length)], padding:'10%'}}>
       <h1 style={{textAlign:'center'}}>{title}</h1>
       <p style={{ textAlign: 'center'}}>{parragraph}</p>
     </div>
@@ -12,7 +12,7 @@ const containerExample = () => {
 
   const containerProps = {
     title: 'Este es un div con background-color random',
-    parragraph: 'Y fuente random'
+    parragraph: 'Y fuente random :D'
   };
 
   ReactDOM.render(
